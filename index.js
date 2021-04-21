8745b6e1c05c19e016e05cec500c87717e227b2d
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-04-21 11:34:09 -0400

    Defined interface for todo response

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'http://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    const todo = response.data;
+
+    const id = todo.id;
+    const title = todo.title;
+    const finished = todo.finished;
+
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished: ${finished}
+    `)
 });
